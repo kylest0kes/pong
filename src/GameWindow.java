75276226d3d5b5e235048950b378cb676f9ff3e5
kylest0kes <kylestokes1551@gmail.com>
2021-03-16 16:43:11 -0400

    program works to open a small window when ran.

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -4,9 +4,16 @@ import java.util.*;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
-    GamePanel panel = new GamePanel();
+    GamePanel panel;
 
     GameWindow() {
-
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
     }
 }
